b9a0206c970145e24b7cf6cb48d0616b4aa98d21
Sebastian Szelest <seba3444gaming@gmail.com>
2022-09-30 21:15:56 +0200

    HMM

--- src/core/calculate-svg-dimensions.ts
+++ src/core/calculate-svg-dimensions.ts
@@ -7,13 +7,14 @@ export function calculateSVGDimensions(
   roundHeader: any,
   currentRound: string = ''
 ) {
-  const bracketHeight = numOfRows * rowHeight;
+  const bracketHeight = (numOfRows * rowHeight) > 700 ? (numOfRows * rowHeight) : 700;
+  const bracketWidth = (numOfColumns * columnWidth) > 1400 ? (numOfColumns * columnWidth) : 1400;
 
   const gameHeight =
     bracketHeight +
     canvasPadding * 2 +
     (roundHeader.isShown ? roundHeader.height + roundHeader.marginBottom : 0);
-  const gameWidth = 1400 + canvasPadding * 2;
+  const gameWidth = bracketWidth + canvasPadding * 2;
   const startPosition = [
     currentRound
       ? -(parseInt(currentRound, 10) * columnWidth - canvasPadding * 2)
