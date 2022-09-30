a580918d7b67e5c159417ce2e2a190637d0fb02c
Sebastian Szelest <seba3444gaming@gmail.com>
2022-09-30 19:34:07 +0200

    x

--- src/bracket-single/single-elim-bracket.tsx
+++ src/bracket-single/single-elim-bracket.tsx
@@ -84,7 +84,7 @@ const SingleEliminationBracket = ({
   return (
     <ThemeProvider theme={theme}>
       <SvgWrapper
-        bracketWidth={gameWidth}
+        bracketWidth={1400}
         bracketHeight={gameHeight}
         startAt={startPosition}
       >
